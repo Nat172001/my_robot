08bd61e647d43cddb2e6ba6b5f09070807a9a43f
Nat172001 <natarajsudharsan01@gmail.com>
2024-09-09 10:05:30 -0500

    URDF

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -29,7 +29,30 @@ def generate_launch_description():
         parameters=[params]
     )
 
+    # Create a joint_state_publisher node
+    node_joint_state_publisher = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher',
+        output='screen'
+    )
+    
+    # Declare RViz config file argument
+    rviz_config = DeclareLaunchArgument(
+        'rviz_config',
+        default_value='/home/nataraj/ros2_ws/src/my_robot/rviz/my_robot.rviz',
+        description='Path to RViz config file'
+    )
 
+    # Launch rviz2 node
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2_1',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rviz_config')]  # Load RViz config file
+    )
+    
     # Launch!
     return LaunchDescription([
         DeclareLaunchArgument(
@@ -37,5 +60,10 @@ def generate_launch_description():
             default_value='false',
             description='Use sim time if true'),
 
-        node_robot_state_publisher
+        rviz_config,  # Declare the RViz config argument first
+
+        node_robot_state_publisher,
+        node_joint_state_publisher,
+        rviz2_node
     ])
+
